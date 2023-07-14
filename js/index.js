d0bede256d762c701843355721dfd04b6544b895
Ruben <rubendns@gmail.com>
2023-07-14 09:48:38 -0300

    cambios

--- js/index.js
+++ js/index.js
@@ -14,63 +14,74 @@ function verCuotas() {
     }
 }
 
-function verPrecio(cod) {
-    switch (cod) {
-        case "1":
-            console.log("Bateria iPhone 7: $ 11500");
-            alert("La bateria del iPhone 7 cuesta: $ 11500");
-            break;
-        case "2":
-            console.log("Bateria iPhone 7 PLUS: $ 12000");
-            alert("La bateria del iPhone PLUS cuesta: $ 12000");
-            break;
-        case "3":
-            console.log("Bateria iPhone 8: $ 12000");
-            alert("La bateria del iPhone 8 cuesta: $ 12000");
-            break;
-        case "4":
-            console.log("Bateria iPhone 8 PLUS: $ 13000");
-            alert("La bateria del iPhone 8 PLUS cuesta: $ 13000");
-            break;
-        case "5":
-            console.log("Bateria iPhone X: $ 16000");
-            alert("La bateria del iPhone X cuesta: $ 16000");
-            break;
-        case "6":
-            console.log("Bateria iPhone XS: $ 17000");
-            alert("La bateria del iPhone XScuesta: $ 17000");
-            break;
-        case "7":
-            console.log("Bateria iPhone XS MAX: $ 19000");
-            alert("La bateria del iPhone XS MAX cuesta: $ 19000");
-            break;
-        case "8":
-            console.log("Bateria iPhone XR: $ 18000");
-            alert("La bateria del iPhone XR cuesta: $ 18000");
-            break;
-        case "9":
-            console.log("Bateria iPhone 11: $ 25000");
-            alert("La bateria del iPhone 11 cuesta: $ 25000");
-            break;
-        case "10":
-            console.log("Bateria iPhone 13: $ 29000");
-            alert("La bateria del iPhone 13 cuesta: $ 29000");
-            break;
-        default:
-            console.warn("Tu selección no es válida, por favor elegí un número del listado");
-            alert("Tu selección no es válida, por favor elegí un número del listado");
+const misBaterias = {
+    "1": {
+        nombre: "Bateria iPhone 7",
+        precio: 11500
+    },
+    "2": {
+        nombre: "Bateria iPhone 7 PLUS",
+        precio: 12000
+    },
+    "3": {
+        nombre: "Bateria iPhone 8",
+        precio: 12000
+    },
+    "4": {
+        nombre: "Bateria iPhone 8 PLUS",
+        precio: 13000
+    },
+    "5": {
+        nombre: "Bateria iPhone X",
+        precio: 16000
+    },
+    "6": {
+        nombre: "Bateria iPhone XS",
+        precio: 17000
+    },
+    "7": {
+        nombre: "Bateria iPhone XS MAX",
+        precio: 19000
+    },
+    "8": {
+        nombre: "Bateria iPhone XR",
+        precio: 18000
+    },
+    "9": {
+        nombre: "Bateria iPhone 11",
+        precio: 25000
+    },
+    "10": {
+        nombre: "Bateria iPhone 13",
+        precio: 29000
     }
-}
+};
 
-function verPrecios() {
-    let respuesta = confirm("¿Deseas conocer el precio de la bateria de algun modelo de iPhone?")
-    if (respuesta === true) {
-        let cod = prompt("Seleccione un numero del listado:");
-        console.log(cod);
-        verPrecio(cod);
+function verProducto(cod) {
+    const bateria = misBaterias[cod];
+    if (bateria) {
+        console.log(`${bateria.nombre}: $ ${bateria.precio}`);
+        alert(`La bateria del ${bateria.nombre} cuesta: $ ${bateria.precio}`);
     } else {
-        console.warn("Dale, No hay problema. Te esperamos cuando gustes :)");
-        alert("Dale, No hay problema. Te esperamos cuando gustes :)");
+        console.warn("Tu selección no es válida, por favor elige un número del listado");
+        alert("Tu selección no es válida, por favor elige un número del listado");
     }
 }
 
+function obtenerPrecio(cod) {
+    const bateria = misBaterias[cod];
+    return `${bateria.precio}`;
+}
+
+function obtenerProductos() {
+    let resultado = '';
+    for (let clave in misBaterias) {
+        resultado += `Clave: ${clave}, Nombre: ${misBaterias[clave].nombre}, Precio: ${misBaterias[clave].precio}\n`;
+    }
+    return resultado;
+}
+
+function bienvenida() {
+    let opc=prompt(`¿Deseas conocer el precio de la bateria de algun modelo de iPhone? \n Seleccione un numero del listado:\n
+    ${obtenerProductos()}`);   
+}
